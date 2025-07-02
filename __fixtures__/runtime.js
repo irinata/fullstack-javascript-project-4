9d8f562f2f3ec6e0466acb99c95c0f317b69b538
Irina Nekrasova <Irina.Tavantseva@gmail.com>
2025-07-02 15:42:04 +0300

    test: add and modify tests, add fixtures

---
+++ __fixtures__/runtime.js
@@ -0,0 +1,10 @@
+export default function filterAnagrams(str, arr) {
+  const makeUniqWord = (word) => String(word)
+    .split("")
+    .map((c) => c.toLowerCase())
+    .sort()
+    .join("");  
+  
+  return arr.filter((item) => makeUniqWord(item)
+    .localeCompare(makeUniqWord(str)) === 0);
+}
--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -11,68 +11,126 @@ const __dirname = path.dirname(__filename)
 
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 
+const mockUrl = 'https://ru.hexlet.io/courses'
+const resourceDir = 'ru-hexlet-io-courses_files'
+const expectedPageName = 'ru-hexlet-io-courses.html'
+const expectedImg1Name = 'ru-hexlet-io-assets-professions-nodejs.png'
+const expectedImg2Name = 'ru-hexlet-io-courses-images-nodejs2.png'
+const expectedCssFileName = 'ru-hexlet-io-assets-application.css'
+const expectedHtmlFileName = 'ru-hexlet-io-courses.html'
+const expectedScriptFileName = 'ru-hexlet-io-packs-js-runtime.js'
+const resourceFilenames = [expectedImg1Name, expectedImg2Name,
+  expectedCssFileName, expectedHtmlFileName, expectedScriptFileName]
+
 nock.disableNetConnect()
 
-it('on non-existent directory should throw exception', async () => {
-  const promise = loadPage('https://ru.hexlet.io/courses', '/tmp/nosuchdir')
-  return expect(promise).rejects.toThrow()
+test('on non-existent directory should throw exceptions', async () => {
+  const noSuchDir = '/tmp/nosuchdir123'
+  const promise = loadPage(mockUrl, noSuchDir)
+  await expect(promise).rejects.toThrow()
+
+  // expect resource dir is not created
+  const promise2 = fsp.stat(path.join(noSuchDir, resourceDir))
+  expect(promise2).rejects.toThrow('ENOENT: no such file or directory')// ENOENT
+
+  // expect page file is not created
+  const promise3 = fsp.stat(path.join(noSuchDir, expectedPageName))
+  expect(promise3).rejects.toThrow('ENOENT: no such file or directory')
 })
 
 describe('check download of page and its resources', () => {
   let initialPageData
   let expectedPageData
-  let img1
-  let img2
+  let expectedCssFile
+  let expectedScriptFile
   let expectedImg1Checksum
   let expectedImg2Checksum
-  let resourceDir = 'ru-hexlet-io-courses_files'
+  let img1
+  let img2
+  let tempDir
 
   beforeAll(async () => {
     initialPageData = await fsp.readFile(getFixturePath('index.html'), 'utf-8')
     expectedPageData = await fsp.readFile(getFixturePath('expected.html'), 'utf-8')
+    expectedCssFile = await fsp.readFile(getFixturePath('application.css'), 'utf-8')
+    expectedScriptFile = await fsp.readFile(getFixturePath('runtime.js'), 'utf-8')
     img1 = await fsp.readFile(getFixturePath('nodejs.png'))
     img2 = await fsp.readFile(getFixturePath('nodejs2.png'))
     expectedImg1Checksum = crc32('CRC-32', img1)
     expectedImg2Checksum = crc32('CRC-32', img2)
   })
 
-  test.each([['temp directory'], ['project directory'],
-  ])('data load into %s should work', async (dir) => {
-    const testDir = dir === 'temp directory'
-      ? await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader'))
-      : process.cwd()
-
-    const expectedPagePath = path.join(testDir, 'ru-hexlet-io-courses.html')
-    const expectedImg1Path = path.join(testDir, resourceDir, 'ru-hexlet-io-assets-professions-nodejs.png')
-    const expectedImg2Path = path.join(testDir, resourceDir, 'ru-hexlet-io-courses-images-nodejs2.png')
-
+  beforeEach(async () => {
     nock('https://ru.hexlet.io')
       .get('/courses')
+      .times(2)
       .reply(200, initialPageData)
-
-    nock('https://ru.hexlet.io')
       .get('/assets/professions/nodejs.png')
       .reply(200, img1)
-
-    nock('https://ru.hexlet.io')
       .get('/courses/images/nodejs2.png')
       .reply(200, img2)
+      .get('/assets/application.css')
+      .reply(200, expectedCssFile)
+      .get('/packs/js/runtime.js')
+      .reply(200, expectedScriptFile)
 
-    const actualPagePath = dir === 'temp directory'
-      ? await loadPage('https://ru.hexlet.io/courses', testDir)
-      : await loadPage('https://ru.hexlet.io/courses')
+    tempDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader'))
+  })
 
-    const actualPageData = await fsp.readFile(actualPagePath, 'utf-8')
+  afterEach(() => {
+    nock.cleanAll()
+  })
 
-    expect(actualPagePath).toBe(expectedPagePath)
+  test('download page and resources into provided directory', async () => {
+    const actualPagePathname = await loadPage(mockUrl, tempDir)
+
+    // expect correct page location and name
+    expect(actualPagePathname).toBe(path.join(tempDir, expectedPageName))
+
+    // expect correct page content
+    const actualPageData = await fsp.readFile(actualPagePathname, 'utf-8')
     expect(actualPageData).toBe(expectedPageData)
 
-    const actualImg1 = await fsp.readFile(expectedImg1Path)
-    const actualImg2 = await fsp.readFile(expectedImg2Path)
+    // expect resource directory exists
+    const resDirStats = await fsp.stat(path.join(tempDir, resourceDir))
+    expect(resDirStats.isDirectory()).toBe(true)
+
+    // expect correct resources content
+    const actualImg1 = await fsp.readFile(path.join(tempDir, resourceDir, expectedImg1Name))
+    const actualImg2 = await fsp.readFile(path.join(tempDir, resourceDir, expectedImg2Name))
     const actualImg1Checksum = crc32('CRC-32', actualImg1)
     const actualImg2Checksum = crc32('CRC-32', actualImg2)
+    const actualCssFile = await fsp.readFile(path.join(tempDir, resourceDir, expectedCssFileName), 'utf-8')
+    const actualHtmlFile = await fsp.readFile(path.join(tempDir, resourceDir, expectedHtmlFileName), 'utf-8')
+    const actualScriptFile = await fsp.readFile(path.join(tempDir, resourceDir, expectedScriptFileName), 'utf-8')
 
     expect(actualImg1Checksum).toBe(expectedImg1Checksum)
     expect(actualImg2Checksum).toBe(expectedImg2Checksum)
+    expect(actualCssFile).toBe(expectedCssFile)
+    expect(actualHtmlFile).toBe(initialPageData)
+    expect(actualScriptFile).toBe(expectedScriptFile)
+  })
+
+  test('download page and resources into project directory', async () => {
+    const actualPagePathname = await loadPage(mockUrl)
+    const projectDir = process.cwd()
+
+    // expect correct page location and name
+    expect(actualPagePathname).toBe(path.join(projectDir, expectedPageName))
+
+    // expect correct page content
+    const actualPageData = await fsp.readFile(actualPagePathname, 'utf-8')
+    expect(actualPageData).toBe(expectedPageData)
+
+    // expect resource directory exists
+    const resDirStats = await fsp.stat(path.join(projectDir, resourceDir))
+    expect(resDirStats.isDirectory()).toBe(true)
+
+    // expect resource files exist
+    const promises = resourceFilenames.map((filename) => {
+      return fsp.stat(path.join(projectDir, resourceDir, filename))
+    })
+    const results = await Promise.all(promises)
+    results.forEach(stats => expect(stats.isFile()).toBe(true))
   })
 })
